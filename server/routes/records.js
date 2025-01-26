3191e6cf61f05a6654e430833b8eb85f4b820be0
unknown <zhou991818@gmail.com>
2025-01-27 01:20:45 +0800

    simpple mern stact crud study

--- server/routes/records.js
+++ server/routes/records.js
@@ -1,34 +1,26 @@
 import express from "express";
-
-// This will help us connect to the databse
 import db from "../db/connection.js";
-
-// This help convert the id from string to ObjectId for the _id.
 import { ObjectId } from "mongodb";
 
-// Router is on instance of the express routes.
-//  We use it to define our routes
-// The router will be added as a middleware and will take control of requests starting path /record
 const router = express.Router();
 
-// This section will help you get a list of all the records.
-router.get("/", async (requestAnimationFrame, res) => {
+// Fix: Correct first parameter from requestAnimationFrame to req
+router.get("/", async (req, res) => {
   let collection = await db.collection("records");
   let results = await collection.find({}).toArray();
-  res.send(results).status(200);
+  res.status(200).send(results);
 });
 
-// This section will help you get a single record by id
-router.get("/:id", async (requestAnimationFrame, res) => {
+// Fix: Correct first parameter from requestAnimationFrame to req
+router.get("/:id", async (req, res) => {
   let collection = await db.collection("records");
   let query = { _id: new ObjectId(req.params.id) };
   let result = await collection.findOne(query);
 
-  if (!result) res.send("Not found").status(404);
-  else res.send(result).status(200);
+  if (!result) res.status(404).send("Not found");
+  else res.status(200).send(result);
 });
 
-//This section will help you create a new record.
 router.post("/", async (req, res) => {
   try {
     let newDocument = {
@@ -37,30 +29,30 @@ router.post("/", async (req, res) => {
       level: req.body.level,
     };
     let collection = await db.collection("records");
-    let result = collection.insertOne(newDocument);
-    res.send(result).status(204);
+    let result = await collection.insertOne(newDocument);
+    res.status(201).send(result); // Changed from 204 to 201 for created resource
   } catch (err) {
-    console.errors(err);
+    console.error(err); // Fixed: console.errors to console.error
     res.status(500).send("Error adding record");
   }
 });
 
-
 router.patch("/:id", async (req, res) => {
   try {
+    const query = { _id: new ObjectId(req.params.id) }; // Added missing query definition
     const updates = {
-      $set:{
+      $set: {
         name: req.body.name,
         position: req.body.position,
-        level: req.body.position,
+        level: req.body.level,
       },
     };
 
     let collection = await db.collection("records");
     let result = await collection.updateOne(query, updates);
-    res.send(result).status(200);
+    res.status(200).send(result);
   } catch (err) {
-    console.log(err);
+    console.error(err);
     res.status(500).send("Error updating record");
   }
 });
@@ -69,13 +61,13 @@ router.delete("/:id", async (req, res) => {
   try {
     const query = { _id: new ObjectId(req.params.id) };
 
-    const collectoin = db.collection("records");
+    let collection = await db.collection("records"); // Fixed typo in 'collection'
     let result = await collection.deleteOne(query);
 
-    res.send(result).status(200);
+    res.status(200).send(result);
   } catch (err) {
     console.error(err);
-    res.status(500).send("Error deleting record")
+    res.status(500).send("Error deleting record");
   }
 });
 
